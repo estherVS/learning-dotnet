1b569c831f26725733b9d73a450fd35c2d6e1f16
estherVS <esther.vieirabv@gmail.com>
2025-04-19 16:52:07 -0400

    feat: create stock dto

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -20,7 +21,8 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto());
+
             return Ok(stocks);
         }
 
@@ -31,10 +33,10 @@ namespace api.Controllers
 
             if(stock == null)
             {
-                    return NotFound();
+                return NotFound();
             }
 
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
 
     }
